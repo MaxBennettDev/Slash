c577e9ed0d56728c3231ab8e3f122fbd7bf5441c
Max Bennett <mx.bennett7@gmail.com>
2026-01-08 13:32:37 +0000

    New Project Files
    Created a new project, and migrated all content into that one. In order to fix issues with visual studio not building, and issues as a result of upgrading the original project from an earlier version of Unreal Engine.

--- Source/Slash/Private/Items/Item.cpp
+++ Source/Slash/Private/Items/Item.cpp
@@ -11,13 +11,15 @@ AItem::AItem()
 
 }
 
+// Called when the game starts or when spawned
 void AItem::BeginPlay()
 {
 	Super::BeginPlay();
+	UE_LOG(LogTemp, Warning, TEXT("Item BeginPlay called!"));
 	
-	UE_LOG(LogTemp, Warning, TEXT("Begin play called!"));
 }
 
+// Called every frame
 void AItem::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
